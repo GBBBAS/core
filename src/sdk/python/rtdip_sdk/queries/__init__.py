c21717260c2d8d1d3db9a8e2981b96e7eed8128f
GBBBAS <42962356+GBBBAS@users.noreply.github.com>
2023-08-15 08:45:55 +0100

    Implement Black Code Style
    Signed-off-by: GBBBAS <42962356+GBBBAS@users.noreply.github.com>

--- src/sdk/python/rtdip_sdk/queries/__init__.py
+++ src/sdk/python/rtdip_sdk/queries/__init__.py
@@ -12,4 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .time_series import raw, resample, interpolate, interpolation_at_time, time_weighted_average
+from .time_series import (
+    raw,
+    resample,
+    interpolate,
+    interpolation_at_time,
+    time_weighted_average,
+)
